c31bee8445888ad16bcdee5d4a6583568c4e92be
Marco Marinò <51913759+marcomarinodev@users.noreply.github.com>
2022-09-18 17:12:06 +0200

    Started Multiple Service per Pod smell check

--- freshener/src/k8s_types.rs
+++ freshener/src/k8s_types.rs
@@ -1,27 +1,40 @@
 use serde::{Deserialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct Container {
     pub name: String,
     pub image: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct Volume {
     pub name: String,
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Spec {
+#[derive(Debug, Clone, Deserialize)]
+pub struct TemplateSpec {
     pub initContainers: Option<Vec<Container>>,
     pub containers: Option<Vec<Container>>,
     pub volumes: Option<Vec<Volume>>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
+pub struct Template {
+    pub spec: Option<TemplateSpec>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Spec {
+    pub initContainers: Option<Vec<Container>>,
+    pub containers: Option<Vec<Container>>,
+    pub volumes: Option<Vec<Volume>>,
+    pub template: Option<Template>
+}
+
+#[derive(Debug, Clone, Deserialize)]
 pub struct Metadata { pub name: String }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct K8SManifest {
     pub apiVersion: String,
     pub kind: String,
--- freshener/src/main.rs
+++ freshener/src/main.rs
@@ -1,19 +1,29 @@
 mod k8s_types;
 mod yaml_handler;
 
-use crate::{k8s_types::*, yaml_handler::parse_manifests};
-
+use crate::{k8s_types::K8SManifest};
 
 fn main() {
     let mut manifests: Vec<K8SManifest> = Vec::new();
 
-    parse_manifests(&mut manifests);
+    yaml_handler::parse_manifests(&mut manifests);
 
     println!("[*] Parsing done");
     // let containers = &manifest.spec.containers;
     // if let Some(containers) = containers {
     //     println!("CONTAINERS:\n{:?}", *containers);
     // }
-    println!("{:#?}", manifests);
+
+    check_independent_depl(manifests);
+}
+
+// TODO: Multiple services per container
+fn check_independent_depl(manifests: Vec<K8SManifest>) {
+
+    // filtering manifests, taking only the ones with kind as "Deployment" or "Pod"
+    let deployment_manifests = manifests.iter()
+    .filter(|man| man.kind == "Deployment" || man.kind == "Pod")
+    .collect::<Vec<_>>();
     
-    }
+    println!("{:#?}", deployment_manifests);
+}
