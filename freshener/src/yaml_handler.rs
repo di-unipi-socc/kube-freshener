ec5c87d31dcd3a545725258363ba096044064ec0
Marco Marinò <51913759+marcomarinodev@users.noreply.github.com>
2022-09-18 11:33:00 +0200

    Implemented k8s manifests recursive parsing
    The program is able to recursively search for .yaml files inside the "manifests" folder

--- freshener/src/yaml_handler.rs
+++ freshener/src/yaml_handler.rs
@@ -3,7 +3,7 @@ pub fn unpack(manifest: &String) -> Vec<String> {
         Let's split the manifest using "---"
         as a separator
     */
-    let mut split = manifest.split("---");
+    let split = manifest.split("---");
     let vec = split
         .map(|x| x.to_owned())
         .collect();
