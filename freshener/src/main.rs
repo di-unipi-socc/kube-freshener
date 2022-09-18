b595fa97ab8c567b7fbe29cadd587390bba7f891
Marco Marinò <51913759+marcomarinodev@users.noreply.github.com>
2022-09-18 16:07:21 +0200

    Small refactor on parser
    Separated parser logic from main

--- freshener/src/main.rs
+++ freshener/src/main.rs
@@ -1,41 +1,13 @@
 mod k8s_types;
 mod yaml_handler;
 
-use std::{path::{Path}, fs};
-use crate::k8s_types::*;
-use walkdir::WalkDir;
+use crate::{k8s_types::*, yaml_handler::parse_manifests};
+
 
 fn main() {
     let mut manifests: Vec<K8SManifest> = Vec::new();
 
-    // TODO: read all manifests inside the folder "manifests"
-    // and parse them into manifests vector so that we can analyze them
-
-    for entry in WalkDir::new(".")
-        .follow_links(true)
-        .into_iter()
-        .filter_map(|e| e.ok()) {
-            let filename = entry.file_name().to_string_lossy();
-
-            if filename.ends_with(".yaml") {
-                println!("[*] Parsing {}", filename);
-                let path = entry.path();
-                let ref manifest_string = fs::read_to_string(path)
-                    .expect(&filename.to_string());
-
-                /*
-                Case when we have a manifest which declares different k8s components
-                separated by "---"
-                */ 
-                let ref sub_manifests = yaml_handler::unpack(manifest_string);
-                
-                // deserializing manifests
-                for m in sub_manifests {
-                    let converted_manifest: K8SManifest = serde_yaml::from_str(&m).unwrap();
-                    manifests.push(converted_manifest)
-                }
-            }
-    }
+    parse_manifests(&mut manifests);
 
     println!("[*] Parsing done");
     // let containers = &manifest.spec.containers;
@@ -44,5 +16,4 @@ fn main() {
     // }
     println!("{:#?}", manifests);
     
-}
-
+    }
--- freshener/src/yaml_handler.rs
+++ freshener/src/yaml_handler.rs
@@ -1,4 +1,36 @@
-pub fn unpack(manifest: &String) -> Vec<String> {
+use std::{fs};
+use walkdir::WalkDir;
+use crate::{k8s_types::*, yaml_handler};
+
+pub fn parse_manifests(manifests: &mut Vec<K8SManifest>) {
+    for entry in WalkDir::new(".")
+        .follow_links(true)
+        .into_iter()
+        .filter_map(|e| e.ok()) {
+            let filename = entry.file_name().to_string_lossy();
+
+            if filename.ends_with(".yaml") {
+                println!("[*] Parsing {}", filename);
+                let path = entry.path();
+                let ref manifest_string = fs::read_to_string(path)
+                    .expect(&filename.to_string());
+
+                /*
+                Case when we have a manifest which declares different k8s components
+                separated by "---"
+                */ 
+                let ref sub_manifests = yaml_handler::unpack(manifest_string);
+                
+                // deserializing manifests
+                for m in sub_manifests {
+                    let converted_manifest: K8SManifest = serde_yaml::from_str(&m).unwrap();
+                    manifests.push(converted_manifest)
+                }
+            }
+    }
+}
+
+fn unpack(manifest: &String) -> Vec<String> {
     /*
         Let's split the manifest using "---"
         as a separator
@@ -10,4 +42,3 @@ pub fn unpack(manifest: &String) -> Vec<String> {
 
     vec
 }
-
